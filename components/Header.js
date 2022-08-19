91add90e242ec44e80f70c3c05edd6d8d2093598
irquigley <93766464+irquigley@users.noreply.github.com>
2022-08-19 08:51:17 +0100

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -3,6 +3,7 @@ import Head from 'next/head'
 
 export default function Header({title}) {
   return (
+     <>
      <Head>
         <title>{title}/title>
         <meta name="description" content="Solid Sound FM" />
@@ -11,19 +12,20 @@ export default function Header({title}) {
       
      <div className="header-wrap">
           <div className="header">
-                  <h1><span>Solid Sound</span></h1>
-                  <div className="menu">
-                      <ul>
-                          <li><Link href="/"><a>HOME</a></Link></li>
-                          <li><Link href="kushti"><a>KUSHTI</a></Link></li>
-                          <li><Link href="interviews"><a>TALKING TUNES</a></Link></li>
-                          <li><Link href="guestmixes"><a>GUEST MIX</a></Link></li>
-                          <li><Link href="hard_rave"><a>FREE TUNES</a></Link></li>
-                          <li><Link href="newreleases"><a>MUSIC REVIEW</a></Link></li>
-                          <li><Link href="faq"><a>FAQ</a></Link></li>
-                      </ul>
-                  </div>
+              <h1><span>Solid Sound</span></h1>
+              <div className="menu">
+                  <ul>
+                      <li><Link href="/"><a>HOME</a></Link></li>
+                      <li><Link href="kushti"><a>KUSHTI</a></Link></li>
+                      <li><Link href="interviews"><a>TALKING TUNES</a></Link></li>
+                      <li><Link href="guestmixes"><a>GUEST MIX</a></Link></li>
+                      <li><Link href="hard_rave"><a>FREE TUNES</a></Link></li>
+                      <li><Link href="newreleases"><a>MUSIC REVIEW</a></Link></li>
+                      <li><Link href="faq"><a>FAQ</a></Link></li>
+                  </ul>
+              </div>
             </div>
       </div>
+    </>
   )
 }
