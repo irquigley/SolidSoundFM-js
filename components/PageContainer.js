9dc7141f08a01842d925e8dc368787e4476e2cb5
irquigley <93766464+irquigley@users.noreply.github.com>
2022-08-19 09:14:12 +0100

    Update PageContainer.js

--- components/PageContainer.js
+++ components/PageContainer.js
@@ -1,6 +1,7 @@
 import Link from 'next/link'
 import Script from 'next/script'
 import Header from './Header'
+import GoogleAnalytics from './scripts/GoogleAnalytics'
 
 export default function PageContainer({title, children}) {
   return (
@@ -10,21 +11,7 @@ export default function PageContainer({title, children}) {
      <div className="footer">
         Copyright 2013-{new Date().getFullYear()}. SolidSoundFM.
     </div>
-    
-    <Script id="googleAnalytics"> 
-      {`
-      var _gaq = _gaq || [];
-      _gaq.push(['_setAccount', 'UA-6713202-18']);
-      _gaq.push(['_setDomainName', 'solidsound.fm']);
-      _gaq.push(['_trackPageview']);
-
-      (function() {
-        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-      })();
-      `}
-    </Script>
+    <GoogleAnalytics />    
     </>
   )
 }
