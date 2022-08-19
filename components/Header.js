7ca347ddf3bdf4141783bdc40ee13aaf0b88d532
irquigley <93766464+irquigley@users.noreply.github.com>
2022-08-19 08:37:17 +0100

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,16 +1,20 @@
-import NavButton from '../components/NavButton'
-
 export default function Header() {
   return (    
-    <>
-      <NavButton 
-        title="Two"
-        link="https://google.com" 
-      />    
-      <NavButton 
-        title="Three"
-        link="https://youtube.com" 
-      />
-    </>
+     <div class="header-wrap">
+          <div class="header">
+                  <h1><span>Solid Sound</span></h1>
+                  <div class="menu">
+                      <ul>
+                          <li><a href="/">HOME</a></li>
+                          <li><a href="kushti">KUSHTI</a></li>
+                          <li><a href="interviews">TALKING TUNES</a></li>
+                          <li><a href="guestmixes">GUEST MIX</a></li>
+                          <li><a href="hard_rave">FREE TUNES</a></li>
+                          <li><a href="newreleases">MUSIC REVIEW</a></li>
+                          <li><a href="faq">FAQ</a></li>
+                      </ul>
+                  </div>
+            </div>
+      </div>
   )
 }
