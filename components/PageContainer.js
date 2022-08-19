cad9c5394331f75fb92dfbb2fe487e19453f3c2e
irquigley <93766464+irquigley@users.noreply.github.com>
2022-08-19 09:11:07 +0100

    Update PageContainer.js

--- components/PageContainer.js
+++ components/PageContainer.js
@@ -11,7 +11,8 @@ export default function PageContainer({title, children}) {
         Copyright 2013-{new Date().getFullYear()}. SolidSoundFM.
     </div>
     
-    <Script id="googleAnalytics"> {
+    <Script id="googleAnalytics"> 
+      {`
       var _gaq = _gaq || [];
       _gaq.push(['_setAccount', 'UA-6713202-18']);
       _gaq.push(['_setDomainName', 'solidsound.fm']);
@@ -21,7 +22,8 @@ export default function PageContainer({title, children}) {
         var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
         ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
         var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-      })(); }
+      })();
+      `}
     </Script>
     </>
   )
