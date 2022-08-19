8835faeabf33b0154f83444cc991c7199ee9829a
irquigley <93766464+irquigley@users.noreply.github.com>
2022-08-19 08:44:17 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Header from '../components/Header'
+import PageContainer from '../components/PageContainer'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
