1df7475ce41226ac2c5a67b67e72344794f580f7
irquigley <93766464+irquigley@users.noreply.github.com>
2022-08-19 08:29:10 +0100

    Create Header.js

---
+++ components/Header.js
@@ -0,0 +1,16 @@
+import NavButton from '../components/NavButton'
+
+export default function Header() {
+  return (    
+    <>
+      <NavButton 
+        title="Two"
+        link="https://google.com" 
+      />    
+      <NavButton 
+        title="Three"
+        link="https://youtube.com" 
+      />
+    </>
+  )
+}
