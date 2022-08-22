6a6cb9595521035d5bde0b364a70536110100f27
irquigley <93766464+irquigley@users.noreply.github.com>
2022-08-22 10:06:57 +0100

    Update PageContainer.js

--- components/PageContainer.js
+++ components/PageContainer.js
@@ -1,16 +1,18 @@
 import Link from 'next/link'
 import Script from 'next/script'
 import Header from './Header'
+import Footer from './Footer'
 import GoogleAnalytics from './scripts/googleanalytics'
 
 export default function PageContainer({title, children}) {
   return (
     <>
     <Header title={title}/>
-    <div>{children}</div>
+    <div className="content">{children}</div>
      <div className="footer">
         Copyright 2013-{new Date().getFullYear()}. SolidSoundFM.
     </div>
+    <Footer />
     <GoogleAnalytics />    
     </>
   )
