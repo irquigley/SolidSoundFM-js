6a0cfd4806d64f9218cd0226172d6b57b73489cc
irquigley <93766464+irquigley@users.noreply.github.com>
2022-08-22 10:09:59 +0100

    Create Footer.js

---
+++ components/Footer.js
@@ -0,0 +1,19 @@
+import Link from 'next/link'
+import Head from 'next/head'
+
+export default function Header({title}) {
+  return (
+      <div className="footer">
+          <a
+            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Powered by{' '}
+            <span className={styles.logo}>
+              <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+            </span>
+          </a> -- Copyright 2013-{new Date().getFullYear()}. SolidSoundFM.
+      </div>
+  )
+}
