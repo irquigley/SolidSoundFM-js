c092aa54227f841d3cc7749f305b529dc140cde4
irquigley <93766464+irquigley@users.noreply.github.com>
2022-08-22 10:17:46 +0100

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -1,19 +1,7 @@
-import Link from 'next/link'
-import Image from 'next/image'
-
 export default function Footer({title}) {
   return (
       <div className="footer">
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Powered by{' '}
-            <span className={styles.logo}>
-              <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-            </span>
-          </a> -- Copyright 2013-{new Date().getFullYear()}. SolidSoundFM.
+          Copyright 2013-{new Date().getFullYear()}. SolidSoundFM.
       </div>
   )
 }
