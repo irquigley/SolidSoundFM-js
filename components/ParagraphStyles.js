beaa282fa3f671d42b3a5f7f3b86de3a2522f4c6
irquigley <93766464+irquigley@users.noreply.github.com>
2022-08-22 11:30:54 +0100

    Create ParagraphStyles.js

---
+++ components/ParagraphStyles.js
@@ -0,0 +1,23 @@
+export function ParaMega({children}) {
+  return (
+      <p className="background-mega">{children}</p>
+    )
+}
+
+export function ParaHighlight({children}) {
+  return (
+      <p className="background-highlight">{children}</p>
+    )
+}
+
+export function ParaNormal({children}) {
+  return (
+      <p className="background-page">{children}</p>
+    )
+}
+
+export function ParaLight({children}) {
+  return (
+      <p className="background-lightblue">{children}</p>
+    )
+}
