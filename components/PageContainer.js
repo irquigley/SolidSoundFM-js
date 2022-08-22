b40f7009d1ef6c6c040ae95e746561ace416213c
irquigley <93766464+irquigley@users.noreply.github.com>
2022-08-22 10:08:50 +0100

    Update PageContainer.js

--- components/PageContainer.js
+++ components/PageContainer.js
@@ -8,10 +8,7 @@ export default function PageContainer({title, children}) {
   return (
     <>
     <Header title={title}/>
-    <div className="content">{children}</div>
-     <div className="footer">
-        Copyright 2013-{new Date().getFullYear()}. SolidSoundFM.
-    </div>
+    <div className="content">{children}</div> 
     <Footer />
     <GoogleAnalytics />    
     </>
