8fb663dad92d952b60c473d0d3e0ce4569efbb6b
irquigley <93766464+irquigley@users.noreply.github.com>
2022-08-22 10:30:55 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import HeaderBig from '../components/HeaderBig'
+import Heading1 from '../components/Heading1'
+import Heading2 from '../components/Heading2'
 import PageContainer from '../components/PageContainer'
 import FacebookPlugin from '../components/scripts/facebook'
 
@@ -10,8 +9,9 @@ export default function Home() {
   return (
     <>
        <PageContainer title="Solid Sound FM">
-          <HeaderBig>Header big</HeaderBig>
+          <Header1>Header big</Header1>
           <FacebookPlugin />
+          <Header2>Header small</Header2>
        </PageContainer>
     </>
   )
