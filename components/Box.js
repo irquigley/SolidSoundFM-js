8483a05b0bee3abbff05b0046c37708a3cb5a377
irquigley <93766464+irquigley@users.noreply.github.com>
2022-08-22 15:28:20 +0100

    Update Box.js

--- components/Box.js
+++ components/Box.js
@@ -1,4 +1,4 @@
-export default function Box({title},{children}) {
+export default function Box({title, children}) {
   return (
     <div className="box">
         <div className="title">{title}</div>
