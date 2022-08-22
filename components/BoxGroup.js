559bf06a3184167c337ce2aefff013256944dbe1
irquigley <93766464+irquigley@users.noreply.github.com>
2022-08-22 15:21:50 +0100

    Update BoxGroup.js

--- components/BoxGroup.js
+++ components/BoxGroup.js
@@ -1,6 +1,16 @@
+const COLORS = [
+  "orange",
+  "purple"
+]; 
+
 export default function BoxGroup({color},{children}) {
+  
+  const checkColor = COLORS.includes(color)
+    ? color
+    : COLORS[0];
+  
   return (
-      <div className="boxes {color}">
+      <div className={'boxes ${color}'}>
           {children}
       </div>
   )
