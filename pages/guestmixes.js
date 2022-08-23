01bd01111f58b8a4f158f27d4b5197327444f5a8
irquigley <93766464+irquigley@users.noreply.github.com>
2022-08-23 11:23:30 +0100

    Create guestmixes.js

---
+++ pages/guestmixes.js
@@ -0,0 +1,27 @@
+import Heading1 from '../components/Heading1'
+import Heading2 from '../components/Heading2'
+import ParaMega from '../components/ParaMega'
+import ParaHighlight from '../components/ParaHighlight'
+import ParaLight from '../components/ParaLight'
+import ParaNormal from '../components/ParaNormal'
+import Article from '../components/Article'
+import GapSmall from '../components/GapSmall'
+import GapMedium from '../components/GapMedium'
+import BoxGroup from '../components/BoxGroup'
+import Box from '../components/Box'
+
+import PageContainer from '../components/PageContainer'
+import FacebookPlugin from '../components/scripts/facebook'
+
+import styles from '../styles/Home.module.css'
+
+export default function GuestMixes() {
+  return (
+    <>
+       <PageContainer title="Guest Mixes">
+          <Heading1>Guest Mixes</Heading1>
+          <GapSmall />
+       </PageContainer>
+    </>
+  )
+}
