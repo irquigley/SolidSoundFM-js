eee0f6a6f7e4a079938a5df5d180c127e89dfd12
irquigley <93766464+irquigley@users.noreply.github.com>
2022-08-23 11:20:38 +0100

    Create kushti.js

---
+++ pages/kushti.js
@@ -0,0 +1,36 @@
+import Heading1 from '../components/Heading1'
+import Heading2 from '../components/Heading2'
+import ParaMega from '../components/ParaMega'
+import ParaHighlight from '../components/ParaHighlight'
+import ParaLight from '../components/ParaLight'
+import ParaNormal from '../components/ParaNormal'
+import Article from '../components/Article'
+import GapSmall from '../components/GapSmall'
+import GapMedium from '../components/GapMedium'
+import BoxGroup from '../components/BoxGroup'
+import Box from '../components/Box'
+
+import PageContainer from '../components/PageContainer'
+import FacebookPlugin from '../components/scripts/facebook'
+
+import styles from '../styles/Home.module.css'
+
+export default function Kushti() {
+  return (
+    <>
+       <PageContainer title="Kushti">
+          <Heading1>Kuishti</Heading1>
+          <GapSmall />
+    
+          <Heading2>Header small</Heading2>
+          <Article>          
+            <ParaHighlight>ParaHighlight</ParaHighlight>
+            <ParaLight>ParaLight</ParaLight>
+            <ParaNormal>ParaNormal with gapsmall next</ParaNormal>
+          </Article>
+          <GapSmall />
+    
+       </PageContainer>
+    </>
+  )
+}
