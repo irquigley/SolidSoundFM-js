7ad2f0a7f760c599e900758ad34e5432fc0c8fe2
irquigley <93766464+irquigley@users.noreply.github.com>
2022-08-24 10:26:12 +0100

    First completed homepage

--- components/SoundCloudBox.js
+++ components/SoundCloudBox.js
@@ -1,10 +1,14 @@
-import iframe from 'next/iframe'
-
-export default function SoundCloudBox({link, height}) {
-      if (height === undefined || height === null) {
-        height = 300;
-      }
-      return (
-          <iframe width="100%" height={height} scrolling="no" allow="autoplay" src={link}></iframe>
-      )
+export default function SoundCloudBox({ src, height }) {
+  if (height === undefined || height === null) {
+    height = 300;
+  }
+  return (
+    <iframe
+      width="100%"
+      height={height}
+      scrolling="no"
+      allow="autoplay"
+      src={src}
+    ></iframe>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,3 @@
-import Link from "next/link";
 import Heading1 from "../components/Heading1";
 import Heading2 from "../components/Heading2";
 import ParaMega from "../components/ParaMega";
@@ -10,6 +9,7 @@ import GapSmall from "../components/GapSmall";
 import GapMedium from "../components/GapMedium";
 import BoxGroup from "../components/BoxGroup";
 import Box from "../components/Box";
+import SoundCloudBox from "../components/SoundCloudBox";
 
 import PageContainer from "../components/PageContainer";
 import FacebookPlugin from "../components/scripts/facebook";
@@ -30,19 +30,19 @@ export default function Home() {
         <ParaLight>Established in 2013 and hosted by Kushti.</ParaLight>
         <ParaNormal>
           Every Tuesday on 93.9FM in Dublin,{" "}
-          <a to="https://tunein.com/radio/Dublin-South-FM-939-s135815/">
+          <a href="https://tunein.com/radio/Dublin-South-FM-939-s135815/">
             Tune-In
           </a>{" "}
           and
-          <a to="http://www.DublinSouthFm.ie">Dublin South FM</a>.<br />
+          <a href="http://www.DublinSouthFm.ie">Dublin South FM</a>.<br />
           You will be treated and greeted with some amazing random
           &apos;rave&apos; music.
           <br />
           <br />
-          The main elements are: <a to="/interviews">Interviews</a>,{" "}
-          <a to="/hard_rave">Music Releases</a>,
-          <a to="/guestmixes">Guest Mixes</a> and{" "}
-          <a to="/newreleases">New Music Reviews</a>. As well as any other
+          The main elements are: <a href="/interviews">Interviews</a>,{" "}
+          <a href="/hard_rave">Music Releases</a>,
+          <a href="/guestmixes">Guest Mixes</a> and{" "}
+          <a href="/newreleases">New Music Reviews</a>. As well as any other
           random things that happen as they happen.
         </ParaNormal>
         <GapSmall />
@@ -60,6 +60,10 @@ export default function Home() {
           are the Dublin Rave Community!
         </ParaNormal>
         <GapSmall />
+        <SoundCloudBox
+          height="350"
+          src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/786476694&amp;color=%23444444&amp;auto_play=false&amp;hide_related=false&amp;show_comments=false&amp;show_user=true&amp;show_reposts=false&amp;show_teaser=true"
+        />
       </PageContainer>
     </>
   );
