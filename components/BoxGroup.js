67d7c82389345364920374a2470d9398ead94d48
irquigley <93766464+irquigley@users.noreply.github.com>
2022-08-24 12:27:23 +0100

    talkin tunes update

--- components/BoxGroup.js
+++ components/BoxGroup.js
@@ -1,19 +1,9 @@
-const COLORS = [
-  "orange",
-  "purple"
-]; 
+const COLORS = ["orange", "purple", "pink"];
 
-export default function BoxGroup({color, children}) {
-  
-  const checkColor = COLORS.includes(color)
-    ? color
-    : COLORS[0];
-  
+export default function BoxGroup({ color, children }) {
+  const checkColor = COLORS.includes(color) ? color : COLORS[0];
+
   const combinedClass = "boxes " + checkColor;
-  
-  return (
-      <div className={combinedClass}>
-          {children}
-      </div>
-  )
+
+  return <div className={combinedClass}>{children}</div>;
 }
--- pages/do_talkin_tunes.js
+++ pages/do_talkin_tunes.js
@@ -37,6 +37,166 @@ export default function Do_Talkin_Tunes() {
           Here is the production process. Like most things, it@&apos;s flexible
           and we can figure out what works.
         </ParaNormal>
+
+        <BoxGroup color="orange">
+          <Box title="Step 1. YOU">
+            The guest produces a list of 15 Tunes which you think are worth
+            talking about.
+            <br />
+            <a href="#15tunes">More info below</a>.
+          </Box>
+
+          <Box title="Step 2. ME">
+            I produce a backing track from this list. The Backing Track is just
+            over an hour, and is a SoundCloud private.
+          </Box>
+          <Box title="Step 3. Both">
+            We agree a time, and I&apos;ll call you via Facebook Voice Chat, or
+            Skype, or Whatsapp or any &quot;talk over the internet via
+            computer&quot; application you like.
+          </Box>
+          <Box title="Step 4. Both">
+            Preferably you have a Computer and with either a headset, or
+            headphones and mic set-up.
+          </Box>
+        </BoxGroup>
+
+        <img src="page/talkingtunes-diagram.jpg" className="auto-shrink" />
+
+        <ParaNormal>
+          For your set-up, it would be great if you have Headphones and then
+          listen to the Backing Track on your computer at the same time as me,
+          while we talk. If you have a microphone, that&apos;s great. Or a
+          headset, if that works for you.
+        </ParaNormal>
+
+        <BoxGroup color="orange">
+          <Box title="Step 5.">
+            I spend 4 to 6 hours editing every erm, umm, double talk, misspeak,
+            off-record and gibberish from the voice recording.
+          </Box>
+          <Box title="Step 6.">
+            Finished podcast gets reviewed, then published
+          </Box>
+        </BoxGroup>
+
+        <ParaNormal>
+          After the chat, I bring backing track, my voice and your voice into my
+          DAW. Something magic happens and we have our Talking Tunes podcast.
+        </ParaNormal>
+        <img src="page/tt-reaper.jpg" class="auto-shrink" />
+
+        <Heading2>15 Tunes ... is an hour and a bit</Heading2>
+        <Article>
+          <ParaNormal>
+            We&apos;re aiming for something between an hour to an hour and
+            thirty minutes, which is typically 15 tunes.
+            <br />
+            If all your are short and add up to less than an hour, feel free to
+            add more.
+            <br />
+            If your tunes add up to more than 90 minutes, either cut them down
+            yourself, or I will do that.
+          </ParaNormal>
+          <BoxGroup color="pink">
+            <Box title="Selection">
+              Aim for 80% Rave/Electronic tunes and if you want throw in some
+              curveballs, jazz, classical, metal, folk, whatever, go for it.
+            </Box>
+            <Box title="Max Two">
+              Maximum two tracks &quot;playing this one for someone else&quot;
+              or &quot;reminds me of person&quot; tunes.{" "}
+            </Box>
+            <Box title="Max Two">
+              Maximum two tracks &quot;first track I played as a DJ&quot; or
+              &quot;first tune I remember as a kid&quot; or &quot;when I was in
+              a phase of my life&quot;
+            </Box>
+            <Box title="Random">
+              Pick some tracks that are; under-rated, best song ever, genre
+              changing.
+            </Box>
+            <Box title="You Like It">
+              Select tunes YOU like, and be prepared to discuss the artistic
+              merit of them. Why did you pick this one? Some people say to me
+              &quot;I know you don&apos;t like xyz so I didn&apos;t pick
+              that&quot;. Pick what you like.
+            </Box>
+            <Box title="Send MP3Z">
+              If you can send me the digital files for your selection, that
+              would be super helpful! Use WeTransfer.com for example
+            </Box>
+            <Box title="Notes">
+              Do a little background research. People love hearing nuggets of
+              information. I&apos;ll do the same and share my notes. <br />
+              <font size="-1">
+                33:10 Rachmaninoff - Rhapsody on a Theme of Paganini, Op.43
+                Variation XVIII (1934) Russian [1873 - 1943] Following the
+                Russian Revolution, Rachmaninoff and his family left Russia.
+                They settled in New York City in 1918. Rachmaninoff wrote the
+                work at his summer home, in Switzerland
+              </font>
+            </Box>
+          </BoxGroup>
+        </Article>
+
+        <Heading2>How to Talk</Heading2>
+        <ParaNormal>
+          From a very early age, you probably started talking and never really
+          gave it much thought. Which is normal. This section isn&apos;t
+          important, but it&apos;s helpful to some people and I&apos;ve built up
+          this &apos;hints&apos; guide over time
+        </ParaNormal>
+
+        <BoxGroup color="orange">
+          <Box title="Shy?">
+            If you&apos;re feeling shy or nervous, I have years of experience
+            helping people through that.
+          </Box>
+          <Box title="Edit">
+            I edit the chat. I cut out all the erms, yeah, you know what I mean,
+            &quot;it&apos;s like, it&apos;s like&quot; type stuff to make it
+            sound sharp.
+          </Box>
+          <Box title="Don't Panic">
+            We can pause, rewind and do whatever. If you want to stop to get a
+            drink, look up some info, internet stopped or someone interrupted
+            you. That&apos;s no problem at all.
+          </Box>
+          <Box title="Casual">
+            Imagine we are in the pub, having a drink and one of these tunes
+            comes on. The conversation goes &quot;oh this tune, I love it
+            because... &quot; or &quot;did you know that&quot;. This is what
+            talking tunes is about.
+          </Box>
+          <Box title="Notes">
+            Take notes on yourself. When did you start DJing, producing, doing
+            your thing. Who are your influences, who do you look up to. Any
+            interesting anecdotes from going to raves? We will go into
+            &apos;interview&apos; mode.
+          </Box>
+          <Box title="No Script">
+            There is NO script. If you want to talk about something specific,
+            just be blunt with me and say &quot;I want to talk about my new
+            book&quot; before or during recording. I&apos;ll work that in
+            naturally and edit it
+          </Box>
+          <Box title="Pre-Chat">
+            Pre-chat, and Pre-Chat-chat. I can call you a day or two before we
+            talk, just to talk and say hello (Pre-chat-chat). When I call you on
+            the day, we&apos;ll Pre-chat just to go over stuff, check sound
+            levels etc.
+          </Box>
+          <Box title="Structured">
+            Normal chat is back and forth. However, in this format, it&apos;s
+            you we want to hear so I let you talk and talk... and rarely
+            interrupt. I might use &quot;false ignorance&quot; and ask a stupid
+            question for the sake of conversation &quot;so, what is Drum and
+            Bass?&quot;. If we talked about something &apos;off air&apos; avoid
+            saying &quot;like we talked about before&quot; or &quot;like I said
+            earlier&quot;
+          </Box>
+        </BoxGroup>
       </PageContainer>
     </>
   );
